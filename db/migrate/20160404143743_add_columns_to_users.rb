a40e1e949884d46bc119382199c8eb45a1de8c5f
Takezo Miyamoto <takezo.m.1987@gmail.com>
2016-04-04 23:42:22 +0900

    add user model

---
+++ db/migrate/20160404143743_add_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    add_column :users, :avatar, :string
+    add_column :users, :remote_avatar, :string
+  end
+end
